6e36281c87e103b80cfe129ee801bbdc154d9782
Téo <brisse.teo@gmail.com>
2016-05-19 13:51:20 +0200

    - creation evenement v1

--- src/app/controllers/eventCtrl.js
+++ src/app/controllers/eventCtrl.js
@@ -4,11 +4,125 @@
 'use strict';
 
 
-controllers.controller('EventCtrl',['$scope','$location','ConnexionServ', function($scope, $location,ConnexionServ) {
+controllers.controller('EventCtrl',['ManageFriendsServ','$scope','$location', function(ManageFriendsServ,$scope, $location) {
+
+   
+
+    $scope.selection = [];
+
+    $scope.toggle = function (idx) {
+        var pos = $scope.selection.indexOf(idx);
+        if (pos == -1) {
+            $scope.selection.push(idx);
+        } else {
+            $scope.selection.splice(pos, 1);
+        }
+    };
 
     $scope.submit = function () {
         console.log("Success:");
     }
 
-  
+    $scope.returnProfil = function() {
+        $location.path('/profil');
+    };
+
+    $scope.acceptFriend=function(index){
+        var friend=$scope.friendsRequest[index];
+        ManageFriendsServ.acceptFriend(friend.id).accept()
+            .$promise.then(
+            function success(response) {
+
+                ManageFriendsServ.friendsManager().getFriends()
+                    .$promise
+                    .then(
+                        function success(data) {
+
+                            $scope.friends={};
+                            $scope.friends=data;
+                            $scope.friendsRequest.splice(index,1);
+
+                        },
+                        function error(errorResponse) {
+                            console.log("Erreur raffraichissement des amis");
+                        }
+                    );
+
+            },
+            function error(errorResponse) {
+                console.log("Erreur dans l'acceptation");
+                ManageFriendsServ.acceptFriend(friend.id).accept()
+                    .$promise.then(
+                    function success(response) {
+                    }
+                );
+
+            })};
+
+    $scope.denyFriend=function(index){
+        var friend=$scope.friendsRequest[index];
+        ManageFriendsServ.denyFriend(friend.id).deny()
+            .$promise.then(
+            function success(response) {
+                $scope.friendsRequest.splice(index,1);
+            },
+            function error(errorResponse) {
+                console.log("Erreur refus demande d'ami");
+            })
+    };
+
+
+    $scope.removeFriend=function(index){
+
+        var friend=$scope.friends[index];
+
+        ManageFriendsServ.deleteFriend(friend.id).delete()
+            .$promise
+            .then(
+                function success(response) {
+
+                    $scope.friends.splice(index,1);
+
+                },
+                function error(errorResponse) {
+                    console.log("Erreur dans la suppression");
+                }
+            );
+    };
+
+    ManageFriendsServ.friendsManager().getFriends()
+        .$promise
+        .then(
+            function success(data) {
+                $scope.friends={};
+                $scope.friends=data;
+                getFriendsRequest();
+
+            },
+            function error(errorResponse) {
+
+            }
+        );
+
+
+    function getFriendsRequest(){
+        ManageFriendsServ.getFriendRequests().get()
+            .$promise
+            .then(
+                function success(data) {
+                    $scope.friendsRequest=data;
+
+                },
+                function error(errorResponse) {
+
+                })
+    };
+
+    //var refresh=$interval(getFriendsRequest(),3000);
+
+    $scope.addFriend=function() {
+
+        $location.path('addFriend');
+    };
+
 }]);
---
+++ src/app/services/manageFriendsService.js
@@ -0,0 +1,71 @@
+/**
+ * Created by vincent on 23/04/2016.
+ */
+
+'use strict';
+
+
+services.factory('ManageFriendsServ',['$resource','$cookies',
+
+    function($resource,$cookies){
+        return{
+            friendsManager:function() {
+                return $resource("http://localhost:8080/user/friends", {}, {
+
+                    getFriends: {
+                        method: 'GET',
+                        isArray: true
+                    }
+                });
+            },
+            getFriendRequests:function(){
+                return $resource("http://localhost:8080/friendRequest/get", {}, {
+
+                    get: {
+                        method: 'GET',
+                        isArray: true
+                    }
+                });
+            },
+            deleteFriend:function(idFriend){
+                return $resource("http://localhost:8080/user/friends/delete/"+idFriend, {}, {
+
+                    delete: {
+                        method: 'DELETE',
+                        cache: false
+
+                    }
+                });
+            },
+            acceptFriend:function(idAsker){
+                return $resource("http://localhost:8080/friendRequest/accept/"+idAsker, {}, {
+
+                    accept: {
+                        method: 'GET',
+                        cache: false
+
+                    }
+                });
+            },
+            denyFriend:function(idAsker) {
+                return $resource("http://localhost:8080/friendRequest/deny/" + idAsker, {}, {
+
+                    deny: {
+                        method: 'GET',
+                        cache: false
+
+                    }
+                });
+            },
+            searchUsersByName:function(name,page,size){
+                return $resource("http://localhost:8080/user/search?size="+size+"&page="+page+"&name="+name, {}, {
+
+                    get: {
+                        method: 'GET',
+                        cache: false
+
+                    }
+                });
+            }
+
+        }}]);
