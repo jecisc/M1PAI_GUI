e520f4bb71abcd525e597afe3170451ea84d5b77
aurelien <rousseau.aurelien@hotmai.fr>
2016-05-30 19:08:30 +0200

    Merge branch 'LOT2_Creation_evenement' of https://github.com/jecisc/M1PAI_GUI into LOT2_Creation_evenement
    # Conflicts:
    #	src/app/views/event.html

--- src/app/controllers/eventCtrl.js
+++ src/app/controllers/eventCtrl.js
@@ -4,17 +4,17 @@
 'use strict';
 
 
-controllers.controller('EventCtrl',['ManageFriendsServ','$scope','$location', function(ManageFriendsServ,$scope, $location) {
+controllers.controller('EventCtrl', ['ManageFriendsServ', '$scope', '$location', function (ManageFriendsServ, $scope, $location) {
 
 
     /* Permet de choisir des amis a ajouter a l'evenement a partir de la liste
-    *  Toutes les personnes ajoutées seont dans $scope.selection (c'est l'index de la liste concernant l'ami qui est ajouté) */
+     *  Toutes les personnes ajoutées seont dans $scope.selection (c'est l'index de la liste concernant l'ami qui est ajouté) */
     $scope.selection = [];
     $scope.friendsEvent = [];
     $scope.toggle = function (idx) {
         var pos = $scope.selection.indexOf(idx);
         if (pos == -1) {
-           // $scope.selection.push($scope.friends[idx]);
+            // $scope.selection.push($scope.friends[idx]);
             $scope.selection.push(idx);
         } else {
             $scope.selection.splice(pos, 1);
@@ -23,7 +23,7 @@ controllers.controller('EventCtrl',['ManageFriendsServ','$scope','$location', fu
 
 
     $scope.submit = function () {
-        for (var i = 0 ; i < $scope.selection.length ; i++) {
+        for (var i = 0; i < $scope.selection.length; i++) {
             $scope.friendsEvent.push($scope.friends[$scope.selection[i]]);
         }
         var event = {
@@ -33,11 +33,11 @@ controllers.controller('EventCtrl',['ManageFriendsServ','$scope','$location', fu
             descriptionEvent: $scope.descriptionEvent,
             bedEvent: $scope.bedEvent,
             placeDispoEvent: $scope.placeDispoEvent,
-            friendsEvent : $scope.friendsEvent
+            friendsEvent: $scope.friendsEvent
         };
         console.log(event);
 
-    }
+    };
 
 
     /* Permet de recuperer la liste d'amis associée à la personne qui crée l'evenement */
@@ -45,8 +45,8 @@ controllers.controller('EventCtrl',['ManageFriendsServ','$scope','$location', fu
         .$promise
         .then(
             function success(data) {
-                $scope.friends={};
-                $scope.friends=data;
+                $scope.friends = {};
+                $scope.friends = data;
                 getFriendsRequest();
 
             },
@@ -56,17 +56,17 @@ controllers.controller('EventCtrl',['ManageFriendsServ','$scope','$location', fu
         );
 
 
-    function getFriendsRequest(){
+    function getFriendsRequest() {
         ManageFriendsServ.getFriendRequests().get()
             .$promise
             .then(
                 function success(data) {
-                    $scope.friendsRequest=data;
+                    $scope.friendsRequest = data;
 
                 },
                 function error(errorResponse) {
 
                 })
-    };
+    }
 
 }]);
